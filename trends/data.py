4b42f286c2bd59c54a5f517672627c07315f3dc0
laurent <laurent@ip-10-70-83-2.ec2.internal>
2012-06-06 03:40:20 +0000

    update

--- trends/data.py
+++ trends/data.py
@@ -2,18 +2,20 @@ import json
 
 import exceptions
 
-def parse_post(data): 
+def parse_post(data):
     try:
-      post = {}
-      s = data.split(':<$>:')
-      post['msg'] = data
-      post['author_name'] = s[0]
-      post['author_id'] = int(s[1])
-      post['id'] = int(s[2])
-      post['text'] = s[3]
-      post['retweeted'] = int(s[4])
-      post['coordinates'] = json.loads(s[5])
-      post['time'] = int(s[6])
-    except IndexError, ValueError:
-      raise exceptions.DataError()
-    return post
+        post = {}
+        s = data.split(':<$>:')
+        post['msg'] = data
+        post['author_name'] = s[0]
+        post['author_id'] = int(s[1])
+        post['id'] = int(s[2])
+        post['text'] = s[3]
+        post['retweeted'] = int(s[4])
+        post['coordinates'] = json.loads(s[5])
+        post['time'] = int(s[6])
+        post['sentiment'] = int(s[7])
+        return post
+    except (IndexError, ValueError):
+        raise exceptions.DataError()
+    
